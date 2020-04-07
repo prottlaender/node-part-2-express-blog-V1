433d1f234e1821a6b4e81a6334ded4db70c01634
Patrick Rottlaender <p.rottlaender@icloud.com>
2020-04-07 07:36:21 +0200

    README.md created. blog.js console.log changed

--- blog.js
+++ blog.js
@@ -115,4 +115,4 @@ const server = http.createServer(app);
 // server listen for any incoming requests
 server.listen(3000);
 
-console.log('My node.js web server is alive and running at port 3000')
+console.log('My express web server is alive and running at port 3000')
